f457d2007bd106a1346f76fe1255346bd7fa8317
Low Kian Seong <kianseong@gmail.com>
2015-04-22 07:42:21 +0800

    setup.py: add a description
    Closes #1.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name="lazy_paged_sequence",
     author="Misty De Meo",
+    description=("Simple iterable interface to "
+                 "paged data structures such as REST API clients"),
     author_email="mistydemeo@gmail.com",
     license="AGPL",
     version="0.3",
