908538a24f43894a38aa6f910c3f3ffe7fbfad1b
Holly Becker <becker.holly@gmail.com>
2016-10-28 15:04:30 -0700

    Setup.py: Set python3 classifier

--- setup.py
+++ setup.py
@@ -6,7 +6,13 @@ setup(
     description=("Simple iterable interface to "
                  "paged data structures such as REST API clients"),
     author_email="mistydemeo@gmail.com",
+    url='https://github.com/mistydemeo/lazy_paged_sequence',
     license="AGPL",
     version="0.3",
-    py_modules=["lazy_paged_sequence"]
+    py_modules=["lazy_paged_sequence"],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
 )
